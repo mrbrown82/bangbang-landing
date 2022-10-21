5ebd6aea7ea78ec6886bd96aa79ae69b3a1b2e23
Luca Lo Tito <luca@MacBook-Air-Retina.local>
2022-10-21 18:00:56 +0200

    Initial Commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AppLogo from "@/components/AppLogo.vue";
+</script>
+
+<template>
+  <main class="grid h-screen place-items-center">
+    <AppLogo class="fill-slate-200" width="200" />
+  </main>
+</template>
